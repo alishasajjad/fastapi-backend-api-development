c4a382f6de6420d02e1b77b319435e1767a0c428
alishasajjad <dev.alishasajjad@gmail.com>
2026-08-06 18:58:18 +0500

    feat: implement student CRUD API with path and query parameters

--- hello-fastapi/src/todos/main.py
+++ hello-fastapi/src/todos/main.py
@@ -34,3 +34,117 @@ def helloWorld():
 def updateTodo():
     print("Update todo called")
     return "updateTodo called"
+
+@app.get("/user")
+def helloWorld1():
+    return False
+
+@app.get("/list")
+def list():
+    return [1,2,3]
+
+@app.get("/dict")
+def dictionary():
+    return {"name": "alisha"}
+
+# Dynamic Path
+@app.get("/gettodos/{id}")
+def dynamic_path(id):
+    print("Dynamic Path called with ID:", id)
+    return id
+
+
+# Self calling function
+def sumTwoNumbers(a, b):
+    return a + b
+
+sumTwoNumbers(10,20)
+
+
+# Path Variable
+@app.get("/gettodos/{userName}/{rollNo}")
+def dynamic_path_name(userName, rollNo):
+    print("Dynamic Path called with name and roll no:", userName,id)
+    return userName+rollNo
+
+# Type define
+@app.get("/gettodos/{userName}/{rollNo}")
+def dynamic_path_type(userName: str, rollNo: str):
+    print("Dynamic Path called with name and roll no:", userName,id)
+    return userName + rollNo
+
+
+# Query Parameter
+@app.get("/getQueryParameter")
+def query_Parameter(userName:str, rollNo:str):
+    print("Query Parameter called with name and roll no:", userName, rollNo)
+    return "Query Parameter called with name and roll no"
+
+
+students = [{
+    "userName": "Alisha",
+    "rollNo": 112
+},
+{
+    "userName": "Zarmeen",
+    "rollNo": 373
+}]
+
+# get students
+@app.get("/getStudent")
+def getStudent():
+    return students
+
+# add student
+@app.get("/addStudent")
+def addStudent(userName:str, rollNo:str):
+    global students
+    students.append({"userName": userName, "rollNo": rollNo})
+    return students
+
+# Get Single Student (Path Variable)
+@app.get("/getSingleStudent/{rollNo}")
+def getSingleStudent(rollNo: int):
+    global students
+    for student in students:
+        if student["rollNo"] == rollNo:
+            return student
+    return {"message": "Student not found"}
+
+# Search Student (Query Parameter)
+@app.get("/searchStudent")
+def searchStudent(rollNo: int):
+    for student in students:
+        if student["rollNo"] == rollNo:
+            return student
+
+    return {"message": "Student not found"}
+
+# Update Student
+@app.put("/updateStudent")
+def updateStudent(rollNo: int, userName: str):
+    global students
+    for student in students:
+        if student["rollNo"] == rollNo:
+            student["userName"] = userName
+            return {
+                "message": "Student updated successfully",
+                "student": student
+            }
+    return {"message": "Student not found"}
+
+
+# Delete Student
+@app.delete("/deleteStudent")
+def deleteStudent(rollNo: int):
+    global students
+
+    for student in students:
+        if student["rollNo"] == rollNo:
+            students.remove(student)
+            return {
+                "message": "Student deleted successfully",
+                "students": students
+            }
+
+    return {"message": "Student not found"}
